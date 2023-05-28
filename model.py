b8049ea4df0a14d3066073b6bbb141baa4e609b3
Shamoon Siddiqui <shamoon@develop.io>
2023-05-28 17:15:00 -0400

    Got a basic model

--- model.py
+++ model.py
@@ -1,22 +1,27 @@
+# model.py
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
+import math
 
 class TransformerModel(nn.Module):
-    def __init__(self, input_dim, num_classes, nhead, nhid, nlayers, dropout=0.5):
+    def __init__(self, d_model, nhead, nhid, nlayers, nclasses, dropout=0.5):
         super(TransformerModel, self).__init__()
         self.model_type = 'Transformer'
-        self.pos_encoder = PositionalEncoding(input_dim, dropout)
-        encoder_layers = TransformerEncoderLayer(input_dim, nhead, nhid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.decoder = nn.Linear(input_dim, num_classes)
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = nn.TransformerEncoderLayer(d_model, nhead, nhid, dropout)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, nlayers)
+        self.decoder = nn.Linear(d_model, nclasses)
 
     def forward(self, src):
+        # src tensor shape: (seq_len, batch_size, d_model)
         src = self.pos_encoder(src)
+        # src tensor shape after positional encoding: (seq_len, batch_size, d_model)
         output = self.transformer_encoder(src)
-        output = self.decoder(output)
-        return F.log_softmax(output, dim=-1)
+        # output tensor shape: (seq_len, batch_size, d_model)
+        output = self.decoder(output[-1]) # We only want the last output for classification
+        # output tensor shape after decoder: (batch_size, nclasses)
+        return output
+
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=5000):
@@ -32,5 +37,7 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
 
     def forward(self, x):
+        # x tensor shape: (seq_len, batch_size, d_model)
         x = x + self.pe[:x.size(0), :]
+        # x tensor shape after positional encoding: (seq_len, batch_size, d_model)
         return self.dropout(x)
